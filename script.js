7391d216db4f2ed973b2369019bc89f6fb7c4429
Adityaxletscode <150706824+Adityaxletscode@users.noreply.github.com>
2025-02-03 22:58:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed = new Typed('#element', {
+    strings: ['<i>,and</i> I am a passionate Frontend Web Developer.'],
+    typeSpeed: 50,
+    backSpeed: 30,         
+    backDelay: 15000,       
+    loop: true,             
+    showCursor: false,      
+    contentType: 'html'  
+});
